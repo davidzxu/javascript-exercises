30d3fa5f6de5ab0cb89497b19522f202a3e190f5
David Xu <171293428+davidzxu@users.noreply.github.com>
2024-07-24 14:40:30 -0400

    Complete third JS exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let returnString = "";
+    for (let i = string.length - 1; i > -1; i--) {
+        returnString += string[i];
+    }
+    return returnString;
 };
 
 // Do not edit below this line
