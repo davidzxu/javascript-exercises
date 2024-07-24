2dfa201e215f06f5123483974c5fd5751ddd28d3
David Xu <171293428+davidzxu@users.noreply.github.com>
2024-07-24 16:12:06 -0400

    Complete fifth JS exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    // Outer loop cycles between all potential arguments to be removed
+    for (let i = 0; arguments[i] !== undefined; i++) {
+        // Inner loop cycles between all array elements
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === arguments[i]) {
+                array.splice(j, 1);
+                // This ensures two consecutive equal values will both be removed
+                j--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let sum = 0;
+    let lowerValue = 0;
+    let higherValue = 0;
+    if (Number.isInteger(a) && Number.isInteger(b) && a > 0 && b > 0) {
+        if (a < b) {
+            lowerValue = a;
+            higherValue = b;
+        } else {
+            lowerValue = b;
+            higherValue = a;
+        }
+        for (lowerValue; lowerValue !== higherValue + 1; lowerValue++) {
+            sum += lowerValue;
+        }
+    } else {
+        sum = "ERROR";
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
