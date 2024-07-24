fa79183d20aa8c4f95a260f1d97e3a2093e15cf8
David Xu <171293428+davidzxu@users.noreply.github.com>
2024-07-24 14:30:07 -0400

    Complete second JS exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (text, numOfRepeats) {
+    let returnText = "";
+    if (numOfRepeats < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < numOfRepeats; i++) {
+            returnText += text;
+        }
+        return returnText;
+    }
 };
 
 // Do not edit below this line
