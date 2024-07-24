a4a422e457d4ffcc8f607e81583f456747dba4fc
David Xu <171293428+davidzxu@users.noreply.github.com>
2024-07-24 16:40:04 -0400

    Complete seventh JS exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInF) {
+    let tempInC = Math.round((tempInF - 32) * (5 / 9) * 10) / 10;
+    return tempInC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInC) {
+    let tempInF = Math.round(((tempInC * 9) / 5 + 32) * 10) / 10;
+    return tempInF;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
