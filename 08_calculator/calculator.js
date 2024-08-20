9ed53b94e01654faf76c360faddb34b74813f08a
David Xu <171293428+davidzxu@users.noreply.github.com>
2024-08-20 12:07:26 -0400

    Complete 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,52 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+    let value = 0;
+    for (i = 0; i < a.length; i++) {
+        value += a[i];
+    }
+    return value;
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+    let value = 1;
+    for (i = 0; i < a.length; i++) {
+        value *= a[i];
+    }
+    return value;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    let value = a;
+    for (i = 1; i < b; i++) {
+        value *= a;
+    }
+    return value;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    let value = 1;
+    if (a === 0) {
+    } else {
+        for (i = a; i > 0; i--) {
+            value *= i;
+        }
+    }
+    return value;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
