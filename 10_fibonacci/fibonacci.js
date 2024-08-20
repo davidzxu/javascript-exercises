db2de9daeb3552d2b8fa31c44531f737de74c5a6
David Xu <171293428+davidzxu@users.noreply.github.com>
2024-08-20 15:47:19 -0400

    Complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+    let a = 1;
+    let b = 1;
+    let newValue;
+    x = parseInt(x);
+    if (x < 0) {
+        return "OOPS";
+    } else if (x === 0) {
+        return 0;
+    } else if (x === 1 || x === 2) {
+        return 1;
+    } else {
+        for (i = 0; i < x - 2; i++) {
+            newValue = a + b;
+            b = a;
+            a = newValue;
+        }
+        return newValue;
+    }
 };
 
 // Do not edit below this line
